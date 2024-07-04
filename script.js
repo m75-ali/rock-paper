100e2f95e0d0ce4257f50487a74d7d569b94b8e3
m75-ali <116349197+m75-ali@users.noreply.github.com>
2024-07-04 08:40:08 +0100

    Psuedocode added

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+/*First we will get the computers choice 
+Then we will get the humans choice
+Then we initialize the scores for the human and computer at 0
+Then we will play a round of rock, paper, scissors
+If the human chooses rock and the computer chooses scissors, the human wins and the human score increases by 1 and so forth for all the other possible outcomes
+If the human and computer choose the same thing, it is a tie
+If the human chooses something other than rock, paper, or scissors, it is an invalid choice
+We will play 5 rounds of rock, paper, scissors
+After 5 rounds, we will determine the winner based on the scores
+If the human has a higher score than the computer, the human wins
+If the computer has a higher score than the human, the computer wins
+If the human and computer have the same score, it is a tie
+*/
+
+
+
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random()*3)
     switch(randomNumber){
